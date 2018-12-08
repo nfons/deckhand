97155f7e77d8b47777395f448489711b5bfa9ffd
Nate Fonseka <Nate.Fonseka@nike.com>
2018-12-08 14:31:52 -0500

    formatting and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Deck Hand
+[![Go Report Card](https://goreportcard.com/badge/github.com/nfons/deckhand)](https://goreportcard.com/report/github.com/nfons/deckhand)
+
+Kubernetes State saver for GitOps like functionality.
+
+More info to follow
--- deckhand.go
+++ deckhand.go
@@ -2,24 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github.com/ghodss/yaml" //this is better than regular yaml
 	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/ghodss/yaml" //this is better than regular yaml
 	"k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/tools/clientcmd"
-	"log"
-	"os"
-	"path/filepath"
 )
 
 func main() {
-	//set kubeconfig, probably will disable this later
+	// set kubeconfig, probably will disable this later
 	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
 
+	gitUrl := os.Getenv("DECK_GIT_REPO")
+	log.Println("Using Git Repo: " + gitUrl)
 
-	//Get Cluster Name
+	// Get Cluster Name
 	cluserName := os.Getenv("DECK_CLUSTER_NAME")
 
 	if cluserName == "" {
@@ -28,9 +31,9 @@ func main() {
 
 	createPath := filepath.Join("states", cluserName)
 
-	//check if the cluster named folder exists
+	// check if the cluster named folder exists
 	if _, err := os.Stat(createPath); os.IsNotExist(err) {
-		//need to create this path
+		// need to create this path
 		os.MkdirAll(createPath, 0777)
 	}
 
@@ -48,30 +51,29 @@ func main() {
 
 	namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
 
-	//for each namespace, we will iterate and get deployments
-	for _, val := range namespaces.Items{
+	// for each namespace, we will iterate and get deployments
+	for _, val := range namespaces.Items {
 		log.Printf(val.Name)
 		Deployments, err := clientset.AppsV1().Deployments(val.Name).List(metav1.ListOptions{})
-		//we should probably also get rs, ss, and ds as those would change as well.
-		//we will get them, but leave them blank for now
+		// we should probably also get rs, ss, and ds as those would change as well.
+		// we will get them, but leave them blank for now
 		clientset.AppsV1().DaemonSets(val.Name).List(metav1.ListOptions{})
 		clientset.AppsV1().StatefulSets(val.Name).List(metav1.ListOptions{})
 		clientset.AppsV1().ReplicaSets(val.Name).List(metav1.ListOptions{})
-
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		//create NS  dir if not exist
+		// create NS  dir if not exist
 		namespacePath := filepath.Join(createPath, val.Name)
 
-		//create the namespace path if it doesn't exist
-		if _,exist := os.Stat(namespacePath); os.IsNotExist(exist) {
+		// create the namespace path if it doesn't exist
+		if _, exist := os.Stat(namespacePath); os.IsNotExist(exist) {
 			os.MkdirAll(filepath.Join(namespacePath), 0777)
 		}
 
-		//save the namespace as well
+		// save the namespace as well
 		namespace := KubeStruct{}
 		namespace.ApiVersion = "v1"
 		namespace.Kind = "Namespace"
@@ -90,26 +92,23 @@ func main() {
 			log.Fatal(writeErr)
 		}
 
-		//save deployments
+		// save deployments
 		SaveDeployments(Deployments.Items, namespacePath)
 
-		//save statefulsets
+		// save statefulsets
 
-
-		//eventually we will need to loop through the ss,ds,rs as well and save those
+		// eventually we will need to loop through the ss,ds,rs as well and save those
 	}
 
 }
 
-
 func SaveDeployments(Deployments []v1.Deployment, path string) {
-	//loop through each deployment and create a a deployment yaml
-	for _, deploy := range Deployments{
-		yamlName := fmt.Sprintf("%s/%s.deploy.yaml",  path, deploy.Name)
+	// loop through each deployment and create a a deployment yaml
+	for _, deploy := range Deployments {
+		yamlName := fmt.Sprintf("%s/%s.deploy.yaml", path, deploy.Name)
 
-		//create kctl deployment struct
+		// create kctl deployment struct
 		deployment := KubeStruct{}
-
 
 		deployment.ApiVersion = "apps/v1"
 		deployment.Kind = "Deployment"
@@ -117,8 +116,8 @@ func SaveDeployments(Deployments []v1.Deployment, path string) {
 		deployment.Metadata = deploy.ObjectMeta
 		deployment.Spec = deploy.Spec
 
-		//save the deployment file
-		marshalYaml, err:= yaml.Marshal(deployment)
+		// save the deployment file
+		marshalYaml, err := yaml.Marshal(deployment)
 		if err != nil {
 			log.Panic(err)
 		}
