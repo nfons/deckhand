b666a0ff9315f4389fd534c2a1ff01ee97a68596
nfons <natefonseka@gmail.com>
2018-12-27 10:24:11 -0500

    updating deploy script to make master re-tag as latest

---
+++ builds/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [$TRAVIS_BRANCH == "master"]; then
+     docker tag deckhand quay.io/nfons/deckhand:latest
+     docker push quay.io/nfons/deckhand:latest
+else
+     docker tag deckhand quay.io/nfons/deckhand:$TRAVIS_BRANCH
+     docker push quay.io/nfons/deckhand:$TRAVIS_BRANCH
+fi
